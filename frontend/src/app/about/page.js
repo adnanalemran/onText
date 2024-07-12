7d7d561ef19340d22f8edb7aa9b2de72887e73f0
adnanalemran <adanalemranonntor@gmail.com>
2024-07-12 17:09:24 +0600

    chore: Update About page with error handling and loading components

--- frontend/src/app/about/page.js
+++ frontend/src/app/about/page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const page = () => {
+    throw new Error("this an error ");
     return (
         <div>
             about page content
--- frontend/src/app/components/provider/Shared/Navbar.js
+++ frontend/src/app/components/provider/Shared/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
       <Link href="/"><nav className='bg-slate-900 p-4 hover:bg-slate-950'>Home</nav></Link>
       <nav  className='bg-slate-900 p-4 hover:bg-slate-950'><Link href="/add-task">Add task</Link></nav>
       <nav className='bg-slate-900 p-4 hover:bg-slate-950'><a href="https://github.com">GitHub</a></nav>
-      <Link href="/about"><nav className='bg-slate-900 p-4 hover:bg-slate-950'>About</nav></Link>
+      <Link href="/about" prefetch={false} ><nav className='bg-slate-900 p-4 hover:bg-slate-950'>About</nav></Link>
     </div>
   );
 }
