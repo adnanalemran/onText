c4fa9fe1131af7ed51778ec5d49731e2d253e298
adnanalemran <adnanalemranontor@gmail.com>
2024-07-14 12:59:20 -0700

    post data

---
+++ backend/app/Http/Controllers/TaskController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json(['status' => 'ok', 'tasks' => $tasks], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'completed' => 'required|boolean',
+            'due_date' => 'required|date_format:Y-m-d',
+        ]);
+        $task = Task::create($request->all());
+
+
+
+        return response()->json(['message' => 'Task created successfully', 'task' => $task,], 201);
+    }
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
