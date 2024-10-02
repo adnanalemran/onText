4aeda1c385344974e2650361c9df3dd4baad019e
adnanalemran <adnanalemranontor@gmail.com>
2024-10-02 11:30:17 +0600

    test2

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-//test
+//test strict mode
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
